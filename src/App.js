3ba18799d6eb9d8364b771c38743365a212601b3
rendani99 <rendania460@gmail.com>
2023-05-27 14:08:53 +0200

    run counter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
   return (
     <div className="App">
       <h1> Austin learning React Interview Questions</h1>
-      <h2>Start wiring code Boy!</h2>
+      <h2>Start writting code Boy!</h2>
+      <p> { counter } </p>
+      <button onclick = { () => {
+        setCounter(counter + 1);
+      } } > increase_Counter </button>
     </div>
   );
 }
